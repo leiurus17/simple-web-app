8de41d41732a16371ef2f5941ae1eb6d40e1ea57
Marlon Monzon <martianworm17@gmail.com>
2019-06-09 10:40:22 +0800

    Vaadin todo app layout
    Don't forget the @Route annotation

--- simple-web-app/src/main/java/com/simple/web/MainView.java
+++ simple-web-app/src/main/java/com/simple/web/MainView.java
@@ -1,11 +1,13 @@
-package com.simple.vaadin;
+package com.simple.web;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
 
+@Route
 public class MainView extends VerticalLayout {
 	
 	public MainView() {
